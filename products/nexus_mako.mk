485203fe24d05ebb9d937bf45e211af96d743681
MRH <nightstorm8x@outlook.com>
2016-02-16 15:07:26 +0700

    update fingerprints

--- products/nexus_mako.mk
+++ products/nexus_mako.mk
@@ -28,6 +28,6 @@ PRODUCT_MANUFACTURER := LGE
 # Device Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=occam \
-    BUILD_FINGERPRINT=google/mako/mako:6.0.1/MMB29M/2419427:user/release-keys \
-    PRIVATE_BUILD_DESC="mako-user 6.0.1 MMB29K 2419427 release-keys"
+    BUILD_FINGERPRINT=google/mako/mako:6.0.1/MMB29Q/2480792:user/release-keys \
+    PRIVATE_BUILD_DESC="mako-user 6.0.1 MMB29Q 2480792 release-keys"
 	
